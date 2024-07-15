2ed0421b10012ab975eba6fe74897a5eac41f5cb
ERICK-KRAMER <kramerfilho@hotmail.com>
2024-07-15 15:11:32 -0300

    feat: test route

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
+import { prismaClient } from "./prisma/prismaClient";
 
 class App {
   private readonly app = express();
@@ -18,7 +19,20 @@ class App {
   routes() {
     this.app.get('/', (request: Request, response: Response) => {
       return response.status(200).send('Hello world!');
-    })
+    });
+
+    this.app.get('/products', async (request: Request, response: Response) => {
+      try {
+        const products = await prismaClient.product.findMany();
+        return response.status(200).json(products);
+      } catch (error) {
+        if (error instanceof Error) {
+          return response.status(400).json({ error: error.message });
+        } else {
+          return response.status(400).json({ error: 'Unexpected error.' });
+        }
+      }
+    });
   }
 
   listen(port: number) {
