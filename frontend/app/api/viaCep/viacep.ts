f889897cf6eb470dc06187bcb0517d61e3901ff5
ERICK-KRAMER <kramerfilho@hotmail.com>
2024-07-23 13:43:34 -0300

    feat: integrated viacep

---
+++ frontend/app/api/viaCep/viacep.ts
@@ -0,0 +1,28 @@
+
+export type ViaCepResponse = {
+  cep: string;
+  logradouro: string;
+  complemento: string;
+  unidade: string;
+  bairro: string;
+  localidade: string;
+  uf: string;
+  ibge: string;
+  gia: string;
+  ddd: string;
+  siafi: string;
+};
+
+
+const GetLocation = async (cep: number) => {
+  const url = `https://viacep.com.br/ws/${cep}/json/`;
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export { GetLocation };
--- frontend/app/cart/page.tsx
+++ frontend/app/cart/page.tsx
@@ -5,13 +5,30 @@ import { ChevronLeft, TimerIcon, User } from "lucide-react";
 import { CartItem } from "./cart-item";
 import { Button } from "../components/button/button-login";
 import { Input } from "../components/input/input";
+import { useForm } from "react-hook-form";
+import { GetLocation, ViaCepResponse } from "../api/viaCep/viacep";
+import { useState } from "react";
 
 export default function Page() {
+  const { register, handleSubmit } = useForm();
+  const [data, setData] = useState<ViaCepResponse>();
+
+  const handleClick = async (formData: any) => {
+    try {
+      const response = await GetLocation(formData.cep);
+      console.log(response);
+      setData(response);
+    } catch (error) {
+      console.error("Failed to fetch location data:", error);
+    }
+  };
 
   return (
     <main className="relative p-8 py-8">
       <div className="mb-16">
-        <Link href={"/"} className="absolute top-5 left-5 p-3 rounded-full shadow-md shadow-gray-400 cursor-pointer"><ChevronLeft className="text-red-300" /></Link>
+        <Link href={"/"} className="absolute top-5 left-5 p-3 rounded-full shadow-md shadow-gray-400 cursor-pointer">
+          <ChevronLeft className="text-red-300" />
+        </Link>
       </div>
 
       <div className="flex gap-2">
@@ -28,13 +45,19 @@ export default function Page() {
 
       <div className="h-px w-full bg-neutral-400 my-10"></div>
 
-      <div className="my-2 flex justify-end items-center">
-        <Input placeholder="Cep" />
-        <Input placeholder="Numero" />
-        <Button className="bg-green-600 flex justify-center items-center">calcular</Button>
-      </div>
+      <form className="my-2 flex justify-end items-center" onSubmit={handleSubmit(handleClick)}>
+        <Input placeholder="Cep" {...register('cep')} />
+        <Input placeholder="Numero" {...register('numero')} />
+        <Button className="bg-green-600 flex justify-center items-center">Calcular</Button>
+      </form>
 
-      {/* fazer integraçao com a api do via cep */}
+      {data && (
+        <div>
+          <p>Logradouro: {data.logradouro}</p>
+          <p>Bairro: {data.bairro}</p>
+          <p>Localidade: {data.localidade}</p>
+        </div>
+      )}
 
       <div className="bg-yellow-200 rounded p-2">
         <span className="flex items-center justify-between">
@@ -52,7 +75,6 @@ export default function Page() {
       </div>
 
       <Button className="bg-green-600 rounded my-4">Submit</Button>
-
     </main>
-  )
-};
+  );
+}
